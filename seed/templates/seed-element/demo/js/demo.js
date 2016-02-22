5e8a531d9f9b3fbd3072e543d36fa52d2aef6e16
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-02-22 18:32:13 +0100

    feat(seed-element/demo/js/demo.js): set component properties after 'WebComponentsReady' event for demo

--- seed/templates/seed-element/demo/js/demo.js
+++ seed/templates/seed-element/demo/js/demo.js
@@ -1,9 +1,14 @@
-<% if (i18n) { %>
-  var selectLang = document.querySelector('select');
+document.addEventListener('WebComponentsReady', function() {
+  // set component properties here
+  var el = document.querySelector('seed-element');
+});
 
-  // Demo i18n
-  selectLang.addEventListener('change', function(){
-    document.documentElement.lang = this.value;
-    I18nMsg.lang = document.documentElement.lang;
-  });
+<% if (i18n) { %>
+var selectLang = document.querySelector('select');
+
+// Demo i18n
+selectLang.addEventListener('change', function(){
+  document.documentElement.lang = this.value;
+  I18nMsg.lang = document.documentElement.lang;
+});
 <% } %>
