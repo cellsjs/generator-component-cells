b976dad6eec878c2d52db6ed53951f031d23daf4
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-03-18 09:34:11 +0100

    feat(seed-element/demo/): set property value via mocks in demo.js

--- seed/templates/seed-element/demo/js/demo.js
+++ seed/templates/seed-element/demo/js/demo.js
@@ -1,6 +1,7 @@
 document.addEventListener('WebComponentsReady', function() {
   // set component properties here
   var el = Polymer.dom(this.root).querySelector('seed-element');
+  el.set('project', {title: 'Cells', image: '../images/cells.svg'});
 });
 
 <% if (i18n) { %>
