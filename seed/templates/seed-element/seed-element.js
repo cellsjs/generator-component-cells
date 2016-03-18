bd54b2d682745931c728072b0475907668aa4fdc
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-03-18 10:33:48 +0100

    feat(seed-element.js): include use strict

--- seed/templates/seed-element/seed-element.js
+++ seed/templates/seed-element/seed-element.js
@@ -1,26 +1,32 @@
-Polymer({
+(function() {
 
-  is: 'seed-element',
-  <% if (i18n) { %>
-  behaviors: [
-    Polymer.i18nBehavior
-  ],
-  <% } %>
-  properties: {
-    /**
-     * Describes the title of the element, but is really just an excuse to
-     * show off JSDoc annotations.
-     *
-     * @type {{title: string, image: string}}
-     */
-     project: {
-       type: Object,
-       value: function() {
-         return {
-           title: 'Cells',
-           image: '../images/cells.svg'
-         };
-       }
-     }
-  }
-});
+  'use strict';
+
+  Polymer({
+
+    is: 'seed-element',
+    <% if (i18n) { %>
+    behaviors: [
+      Polymer.i18nBehavior
+    ],
+    <% } %>
+    properties: {
+      /**
+      * Describes the title of the element, but is really just an excuse to
+      * show off JSDoc annotations.
+      *
+      * @type {{title: string, image: string}}
+      */
+      project: {
+        type: Object,
+        value: function() {
+          return {
+            title: 'Cells',
+            image: '../images/cells.svg'
+          };
+        }
+      }
+    }
+  });
+  
+})();
