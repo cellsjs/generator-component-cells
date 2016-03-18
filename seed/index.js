ae5df21c03c1f69811ea5b8af9114220c91c5ef6
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-03-18 10:43:26 +0100

    style(seed/index.js): include external file for styling of demo

--- seed/index.js
+++ seed/index.js
@@ -150,6 +150,7 @@ module.exports = yeoman.generators.Base.extend({
     this.copy('seed-element.html', this.elementName + '.html', renameElement);
     this.copy('seed-element.scss', this.elementName + '.scss', renameElement);
     this.copy('demo/js/demo.js', 'demo/js/demo.js', renameElement);
+    this.copy('demo/css/demo-styles.html', 'demo/css/demo-styles.html', renameElement);
     this.copy('demo/index.html', 'demo/index.html', renameElement);
     this.copy('images/cells.svg', 'images/cells.svg', renameElement);
 
