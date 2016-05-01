013e64c21921f15d528e792e0d84fc8c150faad9
Kus Camara <kcmr@users.noreply.github.com>
2016-05-01 18:21:33 +0200

    refactor(seed-element): replace demo "project" property by "heading"
    a property called "title" is avoided because is a native html attribute

--- seed/templates/seed-element/seed-element.js
+++ seed/templates/seed-element/seed-element.js
@@ -5,26 +5,19 @@
   Polymer({
 
     is: 'seed-element',
-    <% if (i18n) { %>
+<% if (i18n) { %>
     behaviors: [
       Polymer.i18nBehavior
     ],
-    <% } %>
+<% } %>
     properties: {
       /**
-      * Describes the title of the element, but is really just an excuse to
-      * show off JSDoc annotations.
-      *
-      * @type {Object} project's title and image url
-      */
-      project: {
-        type: Object,
-        value: function() {
-          return {
-            title: 'Cells',
-            image: '../images/cells.svg'
-          };
-        }
+       * Describes the title of the element, but is really just an excuse to
+       * show off JSDoc annotations.
+       */
+      heading: {
+        type: String,
+        value: 'Cells'
       }
     }
   });
