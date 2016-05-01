da79a5121ed116d033237ef705b21cc09f74ee0c
Kus Camara <kcmr@users.noreply.github.com>
2016-05-01 20:53:55 +0200

    style(demo): remove some whitespaces

--- seed/templates/seed-element/demo/js/demo.js
+++ seed/templates/seed-element/demo/js/demo.js
@@ -1,12 +1,9 @@
 document.addEventListener('WebComponentsReady', function() {
-  // set component properties here
   var demo = document.getElementById('demo');
   demo.heading = 'Cells';
-
 <% if (i18n) { %>
   demo.setDocumentLang = function(e) {
     document.documentElement.lang = e.target.value;
     I18nMsg.lang = e.target.value;
-  };
-<% } %>
+  };<% } %>
 });
