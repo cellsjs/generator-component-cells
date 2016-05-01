c5fc9fc8e62b57629a74c83aa737bf725e57dc54
Kus Camara <kcmr@users.noreply.github.com>
2016-05-01 18:25:40 +0200

    refactor(demo): set component property declaratively and include image as light DOM

--- seed/templates/seed-element/demo/js/demo.js
+++ seed/templates/seed-element/demo/js/demo.js
@@ -1,9 +1,7 @@
 document.addEventListener('WebComponentsReady', function() {
   // set component properties here
-  var el = Polymer.dom(this.root).querySelector('seed-element');
-  el.set('project', {title: 'Cells', image: '../images/cells.svg'});
-
   var demo = document.getElementById('demo');
+  demo.heading = 'Cells';
 
 <% if (i18n) { %>
   demo.setDocumentLang = function(e) {
