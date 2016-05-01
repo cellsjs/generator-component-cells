0a14ab0bbd20e55dde2f44f460a68bdad93bcc97
Kus Camara <kcmr@users.noreply.github.com>
2016-05-01 13:38:46 +0200

    refactor(demo): add dom-bind template and change document lang using an annotated event listener

--- seed/templates/seed-element/demo/js/demo.js
+++ seed/templates/seed-element/demo/js/demo.js
@@ -2,14 +2,13 @@ document.addEventListener('WebComponentsReady', function() {
   // set component properties here
   var el = Polymer.dom(this.root).querySelector('seed-element');
   el.set('project', {title: 'Cells', image: '../images/cells.svg'});
-});
+
+  var demo = document.getElementById('demo');
 
 <% if (i18n) { %>
-var selectLang = Polymer.dom(this.root).querySelector('select');
-
-// Demo i18n
-selectLang.addEventListener('change', function(){
-  document.documentElement.lang = this.value;
-  I18nMsg.lang = document.documentElement.lang;
-});
+  demo.setDocumentLang = function(e) {
+    document.documentElement.lang = e.target.value;
+    I18nMsg.lang = e.target.value;
+  };
 <% } %>
+});
