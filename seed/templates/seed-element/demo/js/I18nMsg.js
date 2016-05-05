58a9adb2edd5f78c04c6f187ed6e61ea8bb60bf8
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-05-05 18:12:28 +0200

    fix: avoid error not finding correct locales url at first and move selector of language event to I18nMsg.js

--- seed/templates/seed-element/demo/js/I18nMsg.js
+++ seed/templates/seed-element/demo/js/I18nMsg.js
@@ -1,5 +1,13 @@
-document.addEventListener('HTMLImportsLoaded', function() {
-  // fallback language is english
-  I18nMsg.lang = document.documentElement.lang || 'en';
-  I18nMsg.url = '../locales';
+window.I18nMsg = {};
+window.I18nMsg.url = '../locales';
+
+// Demo i18n
+var selectLang;
+
+document.addEventListener('WebComponentsReady', function() {
+  selectLang = Polymer.dom(this.root).querySelector('.selector');
+  selectLang.addEventListener('change', function() {
+    document.documentElement.lang = this.value;
+    I18nMsg.lang = document.documentElement.lang;
+  });
 });
--- seed/templates/seed-element/demo/js/demo.js
+++ seed/templates/seed-element/demo/js/demo.js
@@ -3,12 +3,5 @@ document.addEventListener('WebComponentsReady', function() {
   var el = Polymer.dom(this.root).querySelector('seed-element');
 });
 
-<% if (i18n) { %>
-var selectLang = Polymer.dom(this.root).querySelector('select');
 
-// Demo i18n
-selectLang.addEventListener('change', function(){
-  document.documentElement.lang = this.value;
-  I18nMsg.lang = document.documentElement.lang;
 });
-<% } %>
