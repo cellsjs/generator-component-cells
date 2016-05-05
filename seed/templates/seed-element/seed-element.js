6af654014274a22aad73e703901e23432de185af
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-05-05 18:08:51 +0200

    feat: simplify component by removing project property completely.

--- seed/templates/seed-element/seed-element.js
+++ seed/templates/seed-element/seed-element.js
@@ -4,28 +4,12 @@
 
   Polymer({
 
-    is: 'seed-element',
-    <% if (i18n) { %>
+    is: 'seed-element'<% if (i18n) { %>,<% } %>
+<% if (i18n) { %>
     behaviors: [
       Polymer.i18nBehavior
-    ],
-    <% } %>
-    properties: {
-      /**
-      * Describes the title of the element, but is really just an excuse to
-      * show off JSDoc annotations.
-      *
-      * @type {Object} project's title and image url
-      */
-      project: {
-        type: Object,
-        value: function() {
-          return {
-            title: 'Cells',
-          };
-        }
-      }
-    }
+    ]
+<% } %>
   });
 
 })();
