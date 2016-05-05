7fe23228713562f21fe46c0bb4e8da7ab09c59cd
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-05-05 18:24:07 +0200

    feat(demo/js/demo.js): use dom-bind in demo.

--- seed/templates/seed-element/demo/js/demo.js
+++ seed/templates/seed-element/demo/js/demo.js
@@ -1,7 +1,11 @@
-document.addEventListener('WebComponentsReady', function() {
-  // set component properties here
-  var el = Polymer.dom(this.root).querySelector('seed-element');
+var templateBind = Polymer.dom(this.root).querySelector('[is=dom-bind]');
+
+// The dom-change event signifies when the template has stamped its DOM.
+templateBind.addEventListener('dom-change', function() {
+  // auto binding template is ready
+  templateBind.set('greeting', 'Try declarative!');
 });
 
-
+document.addEventListener('WebComponentsReady', function() {
+  // set component properties here
 });
