a63423285dfd37f032d504b47d84ad2fad57b0f1
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2016-11-22 12:13:12 +0100

    refactor(ui/seed-element.js): use window

--- seed/templates/ui-component/seed-element.js
+++ seed/templates/ui-component/seed-element.js
@@ -7,7 +7,7 @@
     is: 'seed-element'<% if (i18n) { %>,<% } %>
 <% if (i18n) { %>
     behaviors: [
-      CellsBehaviors.i18nBehavior
+      window.CellsBehaviors.i18nBehavior
     ]
 <% } %>
   });
